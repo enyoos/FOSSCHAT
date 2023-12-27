3e63a14464f4ac35a57352c9f2fc5d2bfa6af6e6
enyoos <lebedevhh@outlook.fr>
2023-12-27 18:39:13 -0500

    the broadcasting is working!

--- Server/server.js
+++ Server/server.js
@@ -1,36 +1,36 @@
 import { Server } from "socket.io";
 
-const URL = "http://localhost:3000";
-const PORT = 4000;
-var   CLIENTS_SOCKET = [];
-
+const URLS = ["http://localhost:3000", "http://localhost:3001"];
 
 // those are also defined in the socket.js file.
 const EVENTS = {
-    "SEND_MESSAGE" : "SEND_MSG",
-    "DELETE_MESSAGE": "DEL_MSG",
+  "SEND_MESSAGE" : "SEND_MSG",
+  "DELETE_MESSAGE": "DEL_MSG",
 }
+
+const PORT = 4000;
 
 const io = new Server({
   cors: {
-    origin: URL
+    origin: URLS
   }
 });
 
+// emitting the stuff
 io.on( 'connection', ( socket ) => {
 
-
-    socket.on(EVENTS["SEND_MESSAGE"], ( msg ) => {
-      console.log ( "someone just sent a message : " + msg );
-    })
-    
-    socket.on( EVENTS["DELETE_MESSAGE"], ( msg ) => {
-      console.log ( "someone wants to delete msg with content : " + msg );
-    })
+  console.log( "incoming connection !" );
+  socket.on(EVENTS["SEND_MESSAGE"], ( msg ) => {
+    console.log ( "someone just sent a message : " + msg );
+    socket.broadcast.emit (EVENTS["SEND_MESSAGE"], msg);
+  })
+  
+  socket.on( EVENTS["DELETE_MESSAGE"], ( msg ) => {
+    console.log ( "someone wants to delete msg with content : " + msg );
+  })
 });
 
-// io.on( 'disconnection', ( socket ) => {
-//     console.log ( "some user is disconnected" );
-// });
 
-io.listen(PORT, () => console.log ( "Listening for incoming connections ..."));
+
+console.log ( "Listening for some connections ..." );
+io.listen(PORT);
